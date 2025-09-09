4c388ec45ecbd5b22c6af68e5f4a108b16a573eb
= <Alaa@ndm.local>
2025-09-09 18:16:19 +0300

    create main Compoents

--- src/app/core/compoents/header/header.component.ts
+++ src/app/core/compoents/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
